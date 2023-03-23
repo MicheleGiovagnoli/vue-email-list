10bd54014803e64ba64f4c11818031a3edcf6ac1
michele <michele.giovagnoli1@gmail.com>
2023-03-23 18:00:58 +0100

    Aggiunta v-cloak

--- js/main.js
+++ js/main.js
@@ -9,12 +9,12 @@ const { createApp } = Vue
     methods:{
     },
     mounted() {
-            for(let i = 0; i < 10; i++){
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then( (response) => {
-                    this.message.push(response.data.response);
-                    console.log(response.data.response);
-                })
-            }console.log(this.message);
+        for(let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then( (response) => {
+                this.message.push(response.data.response);
+                console.log(response.data.response);
+            })
+        }console.log(this.message);
     }
   }).mount('#app')
